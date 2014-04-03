cc50d7a094b73f19aec1cb2d79cb1a5ff17be593
Mitsutoshi Aoe <maoe@foldr.in>
2014-04-03 15:15:02 +0900

    Add some instances for ToValue and remove ToValue ()

--- src/Database/InfluxDB/Encode.hs
+++ src/Database/InfluxDB/Encode.hs
@@ -1,8 +1,14 @@
+{-# LANGUAGE FlexibleInstances #-}
 {-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE TypeSynonymInstances #-}
 module Database.InfluxDB.Encode where
+import Data.Int (Int8, Int16, Int32, Int64)
 import Data.Proxy
 import Data.Vector (Vector)
+import Data.Word (Word8, Word16, Word32)
 import qualified Data.DList as DL
+import qualified Data.Text as T
+import qualified Data.Text.Lazy as TL
 
 import Database.InfluxDB.Types
 
@@ -25,9 +31,6 @@ toSeriesData a = SeriesData
 class ToValue a where
   toValue :: a -> Value
 
-instance ToValue () where
-  toValue _ = Null
-
 instance ToValue Bool where
   toValue = Bool
 
@@ -38,5 +41,38 @@ instance ToValue a => ToValue (Maybe a) where
 instance ToValue Int where
   toValue = Int . fromIntegral
 
+instance ToValue Int8 where
+  toValue = Int . fromIntegral
+
+instance ToValue Int16 where
+  toValue = Int . fromIntegral
+
+instance ToValue Int32 where
+  toValue = Int . fromIntegral
+
+instance ToValue Int64 where
+  toValue = Int
+
+instance ToValue Word8 where
+  toValue = Int . fromIntegral
+
+instance ToValue Word16 where
+  toValue = Int . fromIntegral
+
+instance ToValue Word32 where
+  toValue = Int . fromIntegral
+
+instance ToValue Float where
+  toValue = Float . realToFrac
+
 instance ToValue Double where
   toValue = Float
+
+instance ToValue T.Text where
+  toValue = String
+
+instance ToValue TL.Text where
+  toValue = String . TL.toStrict
+
+instance ToValue String where
+  toValue = String . T.pack
