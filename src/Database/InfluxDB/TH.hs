a3b9c59fe03bbb4cbd22186ae18166983b842c50
Mitsutoshi Aoe <maoe@foldr.in>
2014-04-15 22:53:26 +0900

    Add driveToSeriesData and deriveSeriesData

--- src/Database/InfluxDB/TH.hs
+++ src/Database/InfluxDB/TH.hs
@@ -7,8 +7,8 @@
 
 module Database.InfluxDB.TH
   ( Options(..), defaultOptions
-  -- , deriveSeriesData
-  -- , deriveToSeriesData
+  , deriveSeriesData
+  , deriveToSeriesData
   , deriveFromSeriesData
 
   , stripPrefixLower
@@ -17,7 +17,10 @@ import Control.Applicative
 import Language.Haskell.TH
 import Language.Haskell.TH.Syntax (VarStrictType)
 
+import qualified Data.Vector as V
+
 import Database.InfluxDB.Decode
+import Database.InfluxDB.Encode
 import Database.InfluxDB.Types.Internal (stripPrefixLower)
 
 data Options = Options
@@ -29,27 +32,60 @@ defaultOptions = Options
   { fieldLabelModifier = id
   }
 
--- deriveSeriesData :: Options -> Name -> Q [Dec]
--- deriveSeriesData opts name = (++)
---   <$> deriveToSeriesData opts name
---   <*> deriveFromSeriesData opts name
+deriveSeriesData :: Options -> Name -> Q [Dec]
+deriveSeriesData opts name = (++)
+  <$> deriveToSeriesData opts name
+  <*> deriveFromSeriesData opts name
 
--- deriveToSeriesData :: Options -> Name -> Q [Dec]
--- deriveToSeriesData _opts _name = do
---   return undefined
+deriveToSeriesData :: Options -> Name -> Q [Dec]
+deriveToSeriesData opts name = do
+  info <- reify name
+  case info of
+    TyConI dec -> pure <$> deriveWith toSeriesDataBody opts dec
+    _ -> fail $ "Expected a type constructor, but got " ++ show info
 
 deriveFromSeriesData :: Options -> Name -> Q [Dec]
 deriveFromSeriesData opts name = do
   info <- reify name
   case info of
-    TyConI dec -> pure <$> decToFromSeriesData opts dec
+    TyConI dec -> pure <$> deriveWith fromSeriesDataBody opts dec
     _ -> fail $ "Expected a type constructor, but got " ++ show info
 
-decToFromSeriesData :: Options -> Dec -> Q Dec
-decToFromSeriesData opts dec = case dec of
-  DataD _ tyName tyVars [con] _ -> fromSeriesDataBody opts tyName tyVars con
-  NewtypeD _ tyName tyVars con _ -> fromSeriesDataBody opts tyName tyVars con
+deriveWith
+  :: (Options -> Name -> [TyVarBndr] -> Con -> Q Dec)
+  -> Options -> Dec -> Q Dec
+deriveWith f opts dec = case dec of
+  DataD _ tyName tyVars [con] _ -> f opts tyName tyVars con
+  NewtypeD _ tyName tyVars con _ -> f opts tyName tyVars con
   _ -> fail $ "Expected a data or newtype declaration, but got " ++ show dec
+
+toSeriesDataBody :: Options -> Name -> [TyVarBndr] -> Con -> Q Dec
+toSeriesDataBody opts tyName tyVars con = do
+  case con of
+    RecC conName vars -> InstanceD
+      <$> mapM tyVarToPred tyVars
+      <*> [t| ToSeriesData $(conT tyName) |]
+      <*> deriveDecs conName vars
+    _ -> fail $ "Expected a record, but got " ++ show con
+  where
+    tyVarToPred tv = case tv of
+      PlainTV name -> classP ''FromValue [varT name]
+      KindedTV name _ -> classP ''FromValue [varT name]
+    deriveDecs conName vars = do
+      a <- newName "a"
+      sequence
+        [ funD 'toSeriesColumns
+          [ clause [wildP]
+            (normalB [| V.fromList $(listE columns) |]) []
+          ]
+        , funD 'toSeriesPoints
+          [ clause [varP a]
+            (normalB [| V.fromList $(listE $ map (applyToValue a) vars) |]) []
+          ]
+        ]
+      where
+        applyToValue a (name, _, _) = [| toValue ($(varE name) $(varE a)) |]
+        columns = map (varStrictTypeToColumn opts) vars
 
 fromSeriesDataBody :: Options -> Name -> [TyVarBndr] -> Con -> Q Dec
 fromSeriesDataBody opts tyName tyVars con = do
@@ -57,13 +93,13 @@ fromSeriesDataBody opts tyName tyVars con = do
     RecC conName vars -> instanceD
       (mapM tyVarToPred tyVars)
       [t| FromSeriesData $(conT tyName) |]
-      [deriveParseSeriesData conName vars]
+      [deriveDec conName vars]
     _ -> fail $ "Expected a record, but got " ++ show con
   where
     tyVarToPred tv = case tv of
       PlainTV name -> classP ''FromValue [varT name]
       KindedTV name _ -> classP ''FromValue [varT name]
-    deriveParseSeriesData conName vars = funD 'parseSeriesData
+    deriveDec conName vars = funD 'parseSeriesData
       [ clause [] (normalB deriveBody) []
       ]
       where
